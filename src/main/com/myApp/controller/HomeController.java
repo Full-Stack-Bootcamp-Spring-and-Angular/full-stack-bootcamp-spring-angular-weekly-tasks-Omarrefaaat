9c7f229104f5b3a7402f97ed3550ad130392600b
omar mohamed <omarmohamed9542@gmail.com>
2026-01-16 20:04:28 +0200

    task5
    : wq: wq: wq#

--- src/main/com/myApp/controller/HomeController.java
+++ src/main/com/myApp/controller/HomeController.java
@@ -1,19 +1,30 @@
 package main.com.myApp.controller;
 
+import main.com.myApp.controller.Dao.DatabaseOperation;
+import main.com.myApp.controller.Dao.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.time.LocalDate;
-import java.time.Period;
+import java.sql.SQLException;
 
-import javax.servlet.http.HttpServletRequest;
-import java.util.Locale;
+import java.util.Date;
 
 @Controller
 public class HomeController
 {
+    DatabaseOperation databaseOperation;
+
+    @Autowired
+    public void setDatabaseOperation(DatabaseOperation databaseOperation) {
+        this.databaseOperation = databaseOperation;
+    }
+
+
+
     @RequestMapping("/")
     public String showHomePage()
     {
@@ -21,12 +32,10 @@ public class HomeController
     }
 
     @RequestMapping("/processForm")
-    public String processForm(@RequestParam("year") int year, @RequestParam("month") int month,@RequestParam("day") int day, Model model){
-        LocalDate today = LocalDate.now();
-        LocalDate birthday = LocalDate.of(year,month,day);
-        Period age = Period.between(birthday,today);
-        String result = "your age is " +  age.getYears() + "Year" + age.getMonths() + "Month" + age.getDays() + "day" ;
-        model.addAttribute("result",result);
+    public String processForm(@RequestParam("firstName") String firstName, @RequestParam("lastName") String lastName, @RequestParam("email") String email, @RequestParam("birthDay")@DateTimeFormat(pattern="yyyy-MM-dd") Date birthDay, Model model) throws SQLException {
+        User user = new User(firstName, lastName, email, birthDay);
+        databaseOperation.saveUser(user);
+        model.addAttribute("message", "user has been saved successfully");
         return "resultPage";
     }
 }
